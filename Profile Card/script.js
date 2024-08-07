2ff38f06f7a3d95a599ebe20c31f762738a6fd36
Jens Debuyck <jens.debuyck@outlook.com>
2024-08-07 17:06:35 +0200

    second commit

--- Profile Card/script.js
+++ Profile Card/script.js
@@ -1,3 +1,16 @@
-document.getElementById("follow").onclick = () =>{
+import { annotate } from 'https://unpkg.com/rough-notation?module';
+
+
+document.getElementById("follow").onclick = () => {
+    
     window.alert("You followed Johnny Rogers");
+    
 }
+
+document.getElementById("message").onclick = () => {
+    const e = document.querySelector('span');
+    const annotation = annotate(e, { type: 'underline' });
+    annotation.show();
+    window.alert("Message sent to Johnny Rogers");
+}
+
